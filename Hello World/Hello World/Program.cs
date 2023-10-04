290317effcd6830af9d709cd550cca34ca1bdfbc
GuneAlone <Gladkoskokrostislav@gmail.com>
2023-10-04 23:38:32 +0300

    проверка 132

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    internal class Praktika
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            // проверка 1
+        }
+    }
+}
